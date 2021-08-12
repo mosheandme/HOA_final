7cd26ba7722a479acf16eb90074e77d69416c7c0
mosheandme <mosheandme@gmail.com>
2021-08-12 20:55:58 +0300

    new tenant button open a modal

--- src/Pages/TenantsPage/TenantsPage.jsx
+++ src/Pages/TenantsPage/TenantsPage.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
-import { Button, Container } from 'react-bootstrap';
+import { useState } from 'react';
+import { Button, Container, Modal } from 'react-bootstrap';
 import { Redirect } from 'react-router-dom';
 import "./TenantsPage.css"
 
 function TenantsPage({ activeUser }) {
+    const [showTenantModal,setShowTenantModal]= useState(false)
     if (!activeUser) {
         return <Redirect to="/" />
 
@@ -13,9 +15,31 @@ function TenantsPage({ activeUser }) {
             <Container>
                 <div className="heading">
                     TenantsPage
-                    <Button>Add New Tenant</Button>
+                    <Button  onClick={() => setShowTenantModal(true) }  >Add New Tenant</Button>
                 </div>
             </Container>
+
+
+            <Modal show={showTenantModal} onHide={() => setShowTenantModal(false)}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Modal heading</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={() => setShowTenantModal(false)}>
+                        Close
+                    </Button>
+                    <Button variant="primary" onClick={() => setShowTenantModal(false)}>
+                        Save Changes
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+
+
+
+
+
+
         </div >
     );
 }
