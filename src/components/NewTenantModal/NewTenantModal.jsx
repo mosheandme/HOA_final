92703a9f2f5cb5714008bacabae9be6a892905c5
mosheandme <mosheandme@gmail.com>
2021-08-12 22:07:26 +0300

    controlled components in NewTenantModal

--- src/components/NewTenantModal/NewTenantModal.jsx
+++ src/components/NewTenantModal/NewTenantModal.jsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import { useState } from 'react';
 import { Button, Col, Form, Modal, Row } from 'react-bootstrap';
 
 function NewTenantModal({ show, onClose }) {
+    const [fname, setFname] = useState("");
+    const [lname, setLname] = useState("");
+    const [email, setEmail] = useState("");
+    const [apartment, setApartment] = useState("");
+    const [pwd, setPwd] = useState("");
+
+
+    function createTenent(){
+        console.log(fname, lname, email, apartment, pwd);
+    }
+
+
+
     return (
         <Modal show={show} onHide={onClose} >
             <Modal.Header closeButton>
@@ -14,7 +28,7 @@ function NewTenantModal({ show, onClose }) {
                             Tenant First Name
                         </Form.Label>
                         <Col sm={10}>
-                            <Form.Control type="text" placeholder="Enter New Tenant First Name" />
+                            <Form.Control type="text" value={fname} onChange={e => setFname(e.target.value)} placeholder="Enter New Tenant First Name" />
                         </Col>
                     </Form.Group>
                     <Form.Group as={Row} className="mb-3" controlId="formHorizontalLname">
@@ -22,7 +36,7 @@ function NewTenantModal({ show, onClose }) {
                             Tenant  Last Name
                         </Form.Label>
                         <Col sm={10}>
-                            <Form.Control type="text" placeholder="Enter New Tenant Last Name" />
+                            <Form.Control type="text" value={lname} onChange={e => setLname(e.target.value)} placeholder="Enter New Tenant Last Name" />
                         </Col>
                     </Form.Group>
                     <Form.Group as={Row} className="mb-3" controlId="formHorizontalEmail">
@@ -30,7 +44,7 @@ function NewTenantModal({ show, onClose }) {
                             Tenant Email
                         </Form.Label>
                         <Col sm={10}>
-                            <Form.Control type="email" placeholder="Enter New Tenant Email" />
+                            <Form.Control type="email" value={email} onChange={e => setEmail(e.target.value)} placeholder="Enter New Tenant Email" />
                         </Col>
                     </Form.Group>
                     <Form.Group as={Row} className="mb-3" controlId="formHorizontalApartment">
@@ -38,7 +52,7 @@ function NewTenantModal({ show, onClose }) {
                             Tenant Apartment Number
                         </Form.Label>
                         <Col sm={10}>
-                            <Form.Control type="number" placeholder="Enter New Tenant Apartment Number" />
+                            <Form.Control type="number" value={apartment} onChange={e => setApartment(e.target.value)} placeholder="Enter New Tenant Apartment Number" />
                         </Col>
                     </Form.Group>
                     <Form.Group as={Row} className="mb-3" controlId="formHorizontalPwd">
@@ -46,7 +60,7 @@ function NewTenantModal({ show, onClose }) {
                             Tenant Password
                         </Form.Label>
                         <Col sm={10}>
-                            <Form.Control type="text" placeholder="Enter New Tenant Password" />
+                            <Form.Control type="text" value={pwd} onChange={e => setPwd(e.target.value)} placeholder="Enter New Tenant Password" />
                         </Col>
                     </Form.Group>
 
@@ -57,7 +71,7 @@ function NewTenantModal({ show, onClose }) {
                 <Button variant="secondary" onClick={onClose}>
                     Cancel
                 </Button>
-                <Button variant="primary" onClick={onClose}>
+                <Button variant="primary" onClick={createTenent}>
                     Save New Tenant
                 </Button>
             </Modal.Footer>
